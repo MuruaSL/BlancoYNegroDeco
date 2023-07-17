a5ba80716827edc981ec3b8cfb908cfd67fc302c
Sergio Leonardo Murua <leonardo-murua@hotmail.com.ar>
2023-07-17 14:25:18 -0300

    se agregaron hipervinculos al banner de nombre y al logo

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -8,15 +8,15 @@ return (
     <>
     <Grid templateColumns='1fr 1fr 15fr 2fr'  className="header">
 
-        <GridItem><img
+        <GridItem><a href="../../index.html"><img
         className="iconB&N"
         src="./src/assets/60x60.png"
         alt="iconBlancoYnegroDeco"
-        /></GridItem>
+        /></a></GridItem>
         
         <GridItem><Categories /></GridItem>
         
-        <GridItem><h1 className="headerTitle">Blanco & Negro Deco</h1></GridItem>
+        <GridItem><a href="../../index.html"><h1 className="headerTitle">Blanco & Negro Deco</h1></a></GridItem>
         
         <GridItem><CartWidget /></GridItem>
     </Grid>
