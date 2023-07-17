e66fb4fad388cd877d6ff07da4193ea52dbef378
Sergio Leonardo Murua <leonardo-murua@hotmail.com.ar>
2023-07-17 13:22:05 -0300

    se cambio el dropdown por un menu hamburguesa

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,25 +1,25 @@
 import React from 'react'
-import { Menu,MenuButton,MenuList,MenuItem,Button } from '@chakra-ui/react';
-import { ChevronDownIcon } from '@chakra-ui/icons';
+import { IconButton,Menu,MenuButton,MenuList,MenuItem,Button } from '@chakra-ui/react';
+import { HamburgerIcon } from '@chakra-ui/icons';
 const Categories = () => {
     return (
-    <>
+        <>
         <Menu>
-        <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
+            <MenuButton as={IconButton} icon={<HamburgerIcon />} >
             Menu
-        </MenuButton>
-        <MenuList>
-            <MenuItem className="menuItemCss">Mantas</MenuItem>
-            <MenuItem className="menuItemCss">Frazadas</MenuItem>
-            <MenuItem className="menuItemCss">Sabanas</MenuItem>
-            <MenuItem className="menuItemCss">Acolchados</MenuItem>
-            <MenuItem className="menuItemCss">Ideas para tu hogar</MenuItem>
-            <MenuItem className="menuItemCss">Quienes somos</MenuItem>
-            <MenuItem className="menuItemCss">Que hacemos</MenuItem>
-            <MenuItem className="menuItemCss">Contactanos</MenuItem>
-        </MenuList>
+            </MenuButton>
+            <MenuList>
+                <MenuItem className="menuItemCss">Mantas</MenuItem>
+                <MenuItem className="menuItemCss">Frazadas</MenuItem>
+                <MenuItem className="menuItemCss">Sabanas</MenuItem>
+                <MenuItem className="menuItemCss">Acolchados</MenuItem>
+                <MenuItem className="menuItemCss">Ideas para tu hogar</MenuItem>
+                <MenuItem className="menuItemCss">Quienes somos</MenuItem>
+                <MenuItem className="menuItemCss">Que hacemos</MenuItem>
+                <MenuItem className="menuItemCss">Contactanos</MenuItem>
+            </MenuList>
         </Menu>
-    </>
+        </>
     );
     }
 
