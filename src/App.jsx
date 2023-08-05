03c651ff9583d727933bcbe2d94e34d735a953f8
Sergio Leonardo Murua <leonardo-murua@hotmail.com.ar>
2023-08-05 13:45:31 -0300

    se cambiaron las rutas de las fotos para corroborar ruta absoluta en vercel

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ const App = () => {
     {
       id: "1",
       name: "Pantuflas",
-      image: "../src/assets/articulos/1.png",
+      image: "/src/assets/articulos/1.png",
       description:" Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam modi excepturi quae totam iure? Pariatur numquam aspernatur eius sunt nulla esse voluptatum, tempora labore magni sapiente impedit itaque eligendi quidem!",
       precio: 999,
       stock: 4,
@@ -22,7 +22,7 @@ const App = () => {
     {
       id: "2",
       name: "Manta corderito ",
-      image: "../src/assets/articulos/7.png",
+      image: "/src/assets/articulos/7.png",
       description: "Mantas de corderito doble faz ",
       precio: 777,
       stock: 6,
@@ -31,7 +31,7 @@ const App = () => {
     {
       id: "3",
       name: "Alfombra rectangulares",
-      image: "../src/assets/articulos/2.png",
+      image: "/src/assets/articulos/2.png",
       description: "descripcion",
       precio: 888,
       stock: 5,
@@ -40,7 +40,7 @@ const App = () => {
     {
       id: "4",
       name: "Alfombra redonda",
-      image: "../src/assets/articulos/6.png",
+      image: "/src/assets/articulos/6.png",
       description: "descripcion",
       precio: 777,
       stock: 6,
@@ -49,7 +49,7 @@ const App = () => {
     {
       id: "5",
       name: "Alfombra de telar",
-      image: "../src/assets/articulos/4.png",
+      image: "/src/assets/articulos/4.png",
       description: "descripcion",
       precio: 999,
       stock: 4,
@@ -58,7 +58,7 @@ const App = () => {
     {
       id: "6",
       name: "Alfombra artesanal ",
-      image: "../src/assets/articulos/5.png",
+      image: "/src/assets/articulos/5.png",
       description: "descripcion",
       precio: 999,
       stock: 4,
@@ -67,7 +67,7 @@ const App = () => {
     {
       id: "7",
       name: "Alfombra de telar",
-      image: "../src/assets/articulos/3.png",
+      image: "/src/assets/articulos/3.png",
       description: "descripcion",
       precio: 777,
       stock: 6,
@@ -76,7 +76,7 @@ const App = () => {
     {
       id: "8",
       name: "Manta corderito gris",
-      image: "../src/assets/articulos/8.png",
+      image: "/src/assets/articulos/8.png",
       description: "descripcion",
       precio: 777,
       stock: 6,
@@ -85,7 +85,7 @@ const App = () => {
     {
       id: "9",
       name: "Toalla y toallon",
-      image: "../src/assets/articulos/9.png",
+      image: "/src/assets/articulos/9.png",
       description: " Juego de toalla y toallon de 500gr",
       precio: 777,
       stock: 6,
@@ -94,7 +94,7 @@ const App = () => {
     {
       id: "10",
       name: "Cubrecama Flannel",
-      image: "../src/assets/articulos/10.png",
+      image: "/src/assets/articulos/10.png",
       description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem nam facere hic consectetur, corporis, dolor reprehenderit recusandae incidunt culpa eligendi voluptas magni possimus voluptatibus reiciendis officiis autem placeat mollitia maiores.",
       precio: 777,
       stock: 6,
@@ -103,7 +103,7 @@ const App = () => {
     {
       id: "11",
       name: "Almohadones",
-      image: "../src/assets/articulos/11.png",
+      image: "/src/assets/articulos/11.png",
       description: "descripcion",
       precio: 777,
       stock: 6,
@@ -112,7 +112,7 @@ const App = () => {
     {
       id: "12",
       name: "Mantas rusticas",
-      image: "../src/assets/articulos/12.png",
+      image: "/src/assets/articulos/12.png",
       description: "descripcion",
       precio: 777,
       stock: 6,
