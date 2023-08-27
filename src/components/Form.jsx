11732de7a8fd3f9d216b36ed370686d25974d7a1
Sergio Leonardo Murua <leonardo-murua@hotmail.com.ar>
2023-08-26 23:13:35 -0300

    se agrego toda la funcionalidad y el formulario de contacto previo al paso de temrinar la compra. el mismo indica el numero de orden y vacia el carrito (falta resolver el vaciado de los campos e indicar que ya se envio mediante alguna alerta )

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,8 +1,57 @@
 import React from 'react'
+import {collection, addDoc, getFirestore} from "firebase/firestore"
+import { useState } from 'react'
+import { useShoppingCart } from '../context/ShoppingCartContext'
 
 const Form = () => {
+
+  const {clearCart}= useShoppingCart()
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState(null);
+  const [message, setMessage] = useState("");
+  const { cart } = useShoppingCart();
+  const { cartTotal } = useShoppingCart();
+  const [orderId, setOrderId] = useState("Aun no completo su pedido")
+  const db =getFirestore()
+  const handleSubmit = (e)=> {
+    e.preventDefault()
+    addDoc(orderCollection, order).then(({id})=>
+    setOrderId(id))
+  }
+  console.log(cart)
+  
+  const order = {
+    name,
+    phone,
+    email,
+    message,
+    cart,
+    cartTotal,
+    orderId
+  }
+
+  const orderCollection = collection(db,"order")
+
   return (
-    <div>Form aaaaaaaaaaaaaaaaaaaaaa</div>
+    <>
+      
+      <div className='formContainer'>
+        <form onSubmit={handleSubmit} className="form">
+          <h2 className='formTitle'>CONTACTENOS</h2>
+          <p type="Name:"><input required  className='formInputs' placeholder="Nombre y Apellido" onChange={(e)=>setName(e.target.value)} /></p>
+          <p type="Name:"><input required className='formInputs' placeholder="Telefono celular" onChange={(e)=>setPhone(e.target.value)} /></p>
+          <p type="Email:"><input required className='formInputs' placeholder="Email de contacto" onChange={(e)=>setEmail(e.target.value)}/></p>
+          <p type="Message:"><textarea required className='formInputs' placeholder="Mensaje" onChange={(e)=>setMessage(e.target.value)} /></p>
+          <p>Su identificador de pedido:</p>
+          <p className='OrderIdP'> {orderId} </p>
+          <button onClick={clearCart} className='formSubmitButton' type='submit'>Enviar Pedido</button>
+          <div className='FormspanContainer'>
+            <span className="formSpan">Consultas: +54 9 351 509 0016</span>
+          </div>
+        </form> 
+      </div>
+    </>
   )
 }
 
